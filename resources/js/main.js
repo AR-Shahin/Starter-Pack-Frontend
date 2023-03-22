e6bd98f147cda94ca295937dda4e23bbac4ae6bb
ar-shahin <mdshahinmije96@gmail.com>
2023-03-22 17:53:35 +0600

    change in 2023

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
-
-    
-});
+// const log = (el = "Ok") => console.log(el);
+// const base_url = window.location.origin;
+// const base_url_admin = `${window.location.origin}/admin`;
+// const $$ = (el) => document.querySelector(el);
